e85b70c05aa6a047688cc927d609dc13f83336d5
Carlangas97 <carlos_gil1997@yahoo.com>
2020-08-13 19:05:47 -0600

    google

--- Ionic/App/src/app/Componentes/login/login.component.ts
+++ Ionic/App/src/app/Componentes/login/login.component.ts
@@ -13,4 +13,7 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {}
 
+  logingoogle(){
+  alert('asiendo login');
+  }
 }
--- Ionic/App/src/environments/environment.ts
+++ Ionic/App/src/environments/environment.ts
@@ -6,6 +6,16 @@ export const environment = {
   production: false
 };
 
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDm5Ivtt2nhvAiclc22nwHqTim4UqKi38E',
+  authDomain: 'login-57a2b.firebaseapp.com',
+  databaseURL: 'https://login-57a2b.firebaseio.com',
+  projectId: 'login-57a2b',
+  storageBucket: 'login-57a2b.appspot.com',
+  messagingSenderId: '498074396755',
+  appId: '1:498074396755:web:360ed8c0da7ccabf0dbea6'
+};
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
