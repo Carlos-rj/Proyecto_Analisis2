aff2e1869ed45e5735fb098897d43956de0a2729
Carlos Jiménez <carlosjimenez2286@gmail.com>
2020-08-14 14:54:32 -0600

    Creacion del Server
    Server alojado en AWS con NodeJS y MySQL

---
+++ Server/build/keys.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    database: {
+        host: '3.90.231.102',
+        user: 'Analisis2',
+        password: 'Analisis2',
+        database: 'Market'
+    }
+};
